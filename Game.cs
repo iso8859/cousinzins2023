9cd72368a56f5bbd81e8f37dd66cf37489d93244
Remi THOMAS <remi@xtware.com>
2023-09-09 19:21:57 +0200

    alpha 1

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿namespace cousinzins2023
+{
+    public class Card
+    {
+        public string image { get; set; }
+        public string answer { get; set; }
+
+        public string data { get; set; }
+    }
+    public class Game
+    {
+        public List<Card> cards { get; set; } = new List<Card>()
+        {
+            new Card() { image = "athletisme.jpg", answer = "centre social cocteau"},
+            new Card() { image = "aviron.jpg", answer = ""},
+            new Card() { image = "badminton.jpg", answer = ""},
+            new Card() { image = "basketball.jpg", answer = ""},
+            new Card() { image = "3x3basketball.jpg", answer = ""},
+        };
+    }
+}
