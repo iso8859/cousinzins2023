fe5938ceda30abd0ecad71cd329a7b182c990ffb
Rémi THOMAS <remi@xtware.com>
2023-09-09 15:12:13 +0200

    .

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output -f net7.0
+./dotnet/dotnet publish -c Release -o bin/output -f net7.0
