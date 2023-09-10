9ae312648589c2972fdb82d3ce7ee8bc793f2057
Remi THOMAS <remi@xtware.com>
2023-09-10 12:16:15 +0200

    Version finale

--- Game.cs
+++ Game.cs
@@ -30,22 +30,22 @@ namespace cousinzins2023
     {
         public List<Card> cards { get; set; } = new List<Card>()
         {
-            new Card() { image = "3x3basketball.jpg", answer = "3x3basketball"},
+            new Card() { image = "3x3basketball.jpg", answer = "ESBVA"},
             new Card() { image = "athletisme.jpg", answer = "athletisme"},
-            new Card() { image = "aviron.jpg", answer = "aviron"},
-            new Card() { image = "badminton.jpg", answer = "badminton"},
-            new Card() { image = "basketball.jpg", answer = "basketball"},
-            new Card() { image = "canoe.jpg", answer = "canoe"},
-            new Card() { image = "concours.jpg", answer = "concours"},
-            new Card() { image = "cyclisme_sur_piste.jpg", answer = "cyclisme_sur_piste"},
-            new Card() { image = "escalade.jpg", answer = "escalade"},
-            new Card() { image = "escrime.jpg", answer = "escrime"},
-            new Card() { image = "football.jpg", answer = "football"},
-            new Card() { image = "golf.jpg", answer = "golf"},
-            new Card() { image = "judo.jpg", answer = ""},
-            new Card() { image = "rugby_a_sept.jpg", answer = "judo"},
-            new Card() { image = "tennis.jpg", answer = "tennis"},
-            new Card() { image = "tir.jpg", answer = "tir"}
+            new Card() { image = "aviron.jpg", answer = "AAATV"},
+            new Card() { image = "badminton.jpg", answer = "alpjj"},
+            // new Card() { image = "basketball.jpg", answer = "basketball"},
+            new Card() { image = "canoe.jpg", answer = "soutien pc"},
+            // new Card() { image = "concours.jpg", answer = "concours"},
+            // new Card() { image = "cyclisme_sur_piste.jpg", answer = "cyclisme_sur_piste"},
+            // new Card() { image = "escalade.jpg", answer = "escalade"},
+            // new Card() { image = "escrime.jpg", answer = "escrime"},
+            new Card() { image = "football.jpg", answer = "office de tourisme de villeneuve d'ascq"},
+            //new Card() { image = "golf.jpg", answer = "golf"},
+            new Card() { image = "judo.jpg", answer = "judo club flers sart"},
+            //new Card() { image = "rugby_a_sept.jpg", answer = "judo"},
+            new Card() { image = "tennis.jpg", answer = "la raquette"},
+            new Card() { image = "tir.jpg", answer = "alpjj"}
         };
 
         public string GetAnswers()
